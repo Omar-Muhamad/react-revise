94b6b6347700fb83a24be5a627c13265419cb435
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-01-11 05:53:27 +0200

    Fetch posts with useEffect

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 
 const ChildComponent = () => {
-  return (
-    <h1>Hello world!</h1>
-  )
-}
+  const [post, setPost] = useState({});
 
-export default ChildComponent
+  const fetchPost = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    setPost(await response.json());
+  };
+
+  useEffect(() => {
+      fetchPost();
+  }, []);
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <div className="">{post.body}</div>
+    </>
+  );
+};
+
+export default ChildComponent;
