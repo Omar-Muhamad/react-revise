52f0d2abf3f0cbd86b4ca5357330d702eea22bec
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-01-11 13:02:46 +0200

    Fetch post using useReducer

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,8 +1,54 @@
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  loading: true,
+  error: "",
+  post: {},
+};
+
+const postReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_SUCCESS":
+      return {
+        loading: false,
+        error: "",
+        post: action.payload,
+      };
+
+    case "FETCH_ERROR":
+      return {
+        loading: false,
+        error: "something went wrong",
+        post: {},
+      };
+
+    default:
+      return state;
+  }
+};
+
+const fetchPosts = async () => {
+  const request = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const post = await request.json();
+  return post;
+};
 
 const ChildComponent = () => {
-  return (
-    <h1>Hello world!</h1>
-  )
-}
+  const [state, dispatch] = useReducer(postReducer, initialState);
 
-export default ChildComponent
+  useEffect(() => {
+    const post = fetchPosts();
+    try {
+      if (post) {
+        dispatch({ type: "FETCH_SUCCESS", payload: post.title });
+      }
+    } catch (error) {
+      dispatch({ type: "FETCH_ERROR" });
+    }
+  }, []);
+  return <h1>{state.loading ? "loading" : state.title}
+  {state.error ? state.error : null}
+  </h1>;
+};
+
+export default ChildComponent;
