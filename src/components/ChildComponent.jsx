77b46f232df1f484a62300a960e1b5f41f980241
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-01-15 12:55:46 +0200

    Add useState example (search and render users from array of users)

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+
+const userData = [
+  {
+    name: "Omar",
+    job: "Fullstack developer",
+  },
+  {
+    name: "John",
+    job: "Teacher",
+  },
+  {
+    name: "Jack",
+    job: "Engineer",
+  },
+];
 
 const ChildComponent = () => {
-  return (
-    <h1>Hello world!</h1>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default ChildComponent
+  const handleChange = (e) => {
+    e.preventDefault();
+    const value = e.target.value;
+    const filteredUsers = userData.filter((user) =>
+      user.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setUsers(filteredUsers);
+  };
+
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <input className="border-2" type="text" onChange={handleChange} />
+      <ul>
+        {users.map((user, index) => (
+          <li key={`${user.name}${index}`}>
+            The username is {user.name} and he is a {user.job}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ChildComponent;
