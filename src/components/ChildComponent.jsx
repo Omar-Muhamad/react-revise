0117a4efa04e23efbf015d802ccb81dcf8f31304
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-01-15 12:15:46 +0200

    Fix the issue

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,19 +1,24 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
 const ChildComponent = () => {
   const [post, setPost] = useState({});
 
   const fetchPost = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts/1"
-    );
-    setPost(await response.json());
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/3"
+      );
+      console.log(response);
+      setPost(response.data);
+    } catch (error) {
+      setPost({ body: error.code });
+    }
   };
 
   useEffect(() => {
-      fetchPost();
+    fetchPost();
   }, []);
-
   return (
     <>
       <h1>Hello world!</h1>
